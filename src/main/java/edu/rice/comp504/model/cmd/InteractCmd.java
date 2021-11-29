435b7dcefbbe133d3866efb86f92c1d40212c24d
Darren <2443592990@qq.com>
2021-11-28 23:14:39 -0600

    Implement Ghosts eating Pacman feature.

--- src/main/java/edu/rice/comp504/model/cmd/InteractCmd.java
+++ src/main/java/edu/rice/comp504/model/cmd/InteractCmd.java
@@ -31,9 +31,9 @@ public class InteractCmd implements IPaintObjCmd {
         if (context.getType().equals("pacman")) {
             Pacman pacman = (Pacman) context;
             collideWithDots(pacman);
-            if (collideWithGhost()!=null) {
-                Ghost ghost = collideWithGhost();
-                resetCharacter(); //Pacman dies or eat the ghost.
+            if (collideWithGhost(pacman) != null) {
+                Ghost ghost = collideWithGhost(pacman); //todo check ghost's status.
+                resetCharacters(); //Pacman dies for now. todo pacman can eat ghosts.
             }
         }
 
@@ -46,17 +46,17 @@ public class InteractCmd implements IPaintObjCmd {
         APaintObject[][] grid = PacmanStore.getGrid();
         Point currLoc = pacman.getLoc();
         String type = grid[currLoc.x][currLoc.y].getType();
-        if(type.equals("dot")) {
-            PacmanStore.setScore(PacmanStore.getScore() + ((Dot)grid[currLoc.x][currLoc.y]).score);
-            PacmanStore.addEatenItems((Dot)grid[currLoc.x][currLoc.y]);
+        if (type.equals("dot")) {
+            PacmanStore.setScore(PacmanStore.getScore() + ((Dot) grid[currLoc.x][currLoc.y]).score);
+            PacmanStore.addEatenItems((Dot) grid[currLoc.x][currLoc.y]);
             grid[currLoc.x][currLoc.y] = new EmptyCell(new Point(currLoc.x, currLoc.y));
-        } else if(type.equals("bigDot")) {
-            PacmanStore.setScore(PacmanStore.getScore() + ((BigDot)grid[currLoc.x][currLoc.y]).score);
-            PacmanStore.addEatenItems((BigDot)grid[currLoc.x][currLoc.y]);
+        } else if (type.equals("bigDot")) {
+            PacmanStore.setScore(PacmanStore.getScore() + ((BigDot) grid[currLoc.x][currLoc.y]).score);
+            PacmanStore.addEatenItems((BigDot) grid[currLoc.x][currLoc.y]);
             grid[currLoc.x][currLoc.y] = new EmptyCell(new Point(currLoc.x, currLoc.y));
-        } else if(type.equals("fruit")) {
-            PacmanStore.setScore(PacmanStore.getScore() + ((Fruit)grid[currLoc.x][currLoc.y]).score);
-            PacmanStore.addEatenItems((Fruit)grid[currLoc.x][currLoc.y]);
+        } else if (type.equals("fruit")) {
+            PacmanStore.setScore(PacmanStore.getScore() + ((Fruit) grid[currLoc.x][currLoc.y]).score);
+            PacmanStore.addEatenItems((Fruit) grid[currLoc.x][currLoc.y]);
             grid[currLoc.x][currLoc.y] = new EmptyCell(new Point(currLoc.x, currLoc.y));
             PacmanStore.setFruitAppear(false);
         }
@@ -65,14 +65,37 @@ public class InteractCmd implements IPaintObjCmd {
     /**
      * If Pacman collides with a ghost.
      */
-    private Ghost collideWithGhost() {
+    private Ghost collideWithGhost(Pacman pacman) {
+
+        for (PropertyChangeListener pcl : iCharacters) {
+            if (((APaintObject) pcl).getType().equals("ghost")) {
+                Ghost ghost = (Ghost) pcl;
+                if (ghost.getLoc().equals(pacman.getLoc())) {
+                    return ghost;
+                }
+                //Check the edge case of pacman and ghost switch positions.
+                if (ghost.getLoc().distance(pacman.getLoc()) != 1) continue;
+                int dirDiff = Math.abs(ghost.getDir() - pacman.getDir());
+                if (dirDiff == 2) return ghost;
+            }
+        }
+
         return null;
     }
 
     /**
      * Pacman loses one life, resets all characters.
      */
-    private void resetCharacter() {
-
+    private void resetCharacters() {
+        for (PropertyChangeListener pcl : iCharacters) {
+            ACharacter character = ((ACharacter) pcl);
+            if (character.getType().equals("pacman")) {
+                Pacman pacman = (Pacman) character;
+                int lives = pacman.getLives();
+                pacman.setLives(lives - 1); //todo check if pacman has lives, 0 live alert and refresh game.
+            }
+            Point point = character.getBornLoc();
+            ((ACharacter) pcl).setLoc(point);
+        }
     }
 }
