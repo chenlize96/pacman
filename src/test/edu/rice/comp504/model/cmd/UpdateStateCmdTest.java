907ec7f4a0c6a6d8f9079c18248fe5c412fe4253
unknown <rl751@scarletmail.rutgers.edu>
2021-11-30 01:01:19 +0000

    test

---
+++ src/test/edu/rice/comp504/model/cmd/UpdateStateCmdTest.java
@@ -0,0 +1,22 @@
+package edu.rice.comp504.model.cmd;
+
+import edu.rice.comp504.model.PacmanStore;
+import edu.rice.comp504.model.agent.ACharacter;
+import edu.rice.comp504.model.agent.Ghost;
+import org.junit.Test;
+
+import java.awt.*;
+
+import static org.junit.Assert.*;
+
+public class UpdateStateCmdTest {
+
+    @Test
+    public void execute() {
+//        ACharacter test = new Ghost("ghost", new Point(1,1), new Point(1,1), 0);
+//        UpdateStateCmd test2 = new UpdateStateCmd(PacmanStore.pcs);
+//        test2.execute(test);
+//        ACharacter test3 = new Ghost("pacman", new Point(1,1), new Point(1,1), 0);
+//        test2.execute(test3);
+    }
+}
